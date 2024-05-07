f9d44af75c799565ab598fed91a0a405c071ad6e
lx-kish <urban.ktulhu@gmail.com>
2024-05-07 22:01:54 +1200

    refactor(core): redesign cabins functionality with Compound Component pattern
    - extract Table functionality from CabinTable to the separate Table.jsx compound component;
    
    - redesign Modal.jsx component with Compound Component pattern;
    
    - add ConfirmDelete.jsx component;
    
    - add Menus.jsx compound component;
    
    - extract closing modal window by clicking outside element functionality into useOutsideClick.js hook;
    
    - redesign AddCabin.jsx component to use modal window;
    
    - redesign CabinRow.jsx and CabinTable.jsx components to use Modal, Menus and Table compound components.

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
 import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
 function AddCabin() {
-  const [isOpenModal, setIsOpenModal] = useState(false);
   return (
     <div>
-      <Button onClick={() => setIsOpenModal((isOpenModal) => !isOpenModal)}>
-        Add new cabin
-      </Button>
-      {isOpenModal && (
-        <Modal onClose={() => setIsOpenModal(false)}>
-          <CreateCabinForm onCloseModal={() => setIsOpenModal(false)} />
-        </Modal>
-      )}
+      <Modal>
+        <Modal.Open opens="cabin-form">
+          <Button>Add new cabin</Button>
+        </Modal.Open>
+        <Modal.Window name="cabin-form">
+          <CreateCabinForm />
+        </Modal.Window>
+      </Modal>
     </div>
   );
 }
