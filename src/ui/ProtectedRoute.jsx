f827263909fa6aa84003d0fc0ed639004136d9b3
lx-kish <urban.ktulhu@gmail.com>
2024-05-30 23:48:12 +1200

    feat(core): add authentication and authorization
    - add apiAuth.js service containing all authentication and authorization api calls;
    
    - add useLogin.js custom hook with React Query logic of log in;
    
    - add useUser.js custom hook with React Query logic of authorization and getting user data;
    
    - add LoginForm.jsx component;
    
    - add Login.jsx page component with log in form;
    
    - add ProtectedRoute.jsx component checking if user logged in, and if not redirecting to the login page;
    
    - wrap all the protected routes of the application with ProtectedRoute.jsx component to prevent unauthorized users to reach protected routes;
    
    - add SpinnerMini.jsx UI component;
    
    - add a new heading style to the Heading.jsx component.

---
+++ src/ui/ProtectedRoute.jsx
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+import Spinner from "./Spinner";
+
+import { useUser } from "../features/authentication/useUser";
+import { useNavigate } from "react-router-dom";
+import { useEffect } from "react";
+
+const FullPage = styled.div`
+  height: 100dvh;
+  background-color: var(--color-grey-50);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function ProtectedRoute({ children }) {
+  const navigate = useNavigate();
+
+  // 1. Load the authenticated user
+  const { isLoading, isAuthenticated } = useUser();
+
+  // 2. If there is NO authenticated user, redirect to the /login page
+  useEffect(
+    function () {
+      if (!isAuthenticated && !isLoading) navigate("/login");
+    },
+    [isAuthenticated, isLoading, navigate]
+  );
+
+  // 3. Show a spinner while loading
+  if (isLoading)
+    return (
+      <FullPage>
+        <Spinner />
+      </FullPage>
+    );
+
+  // 4. If there IS authenticated user, render the app
+  if (isAuthenticated) return children;
+}
+
+export default ProtectedRoute;
